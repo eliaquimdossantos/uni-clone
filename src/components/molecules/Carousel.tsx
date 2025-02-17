8c0335940ab9664282b1fe6099ce395d0f213bcf
Eliaquim dos Santos <eliaquimdossantos@gmail.com>
2025-02-17 17:25:22 -0300

    fix: garantir que o carousel tenha autoplay

--- src/components/molecules/Carousel.tsx
+++ src/components/molecules/Carousel.tsx
@@ -3,8 +3,6 @@
 
 import UniclubeSwiper from "./UniclubeSwiper";
 
-
-
 const images = ["/banner1.svg", "/banner2.svg"];
 
 export default function CarouselBanner() {
@@ -12,10 +10,11 @@ export default function CarouselBanner() {
     <UniclubeSwiper
       id="carousel"
       items={images}
+      autoplay={{ delay: 5000 }}
       renderItem={(src, index) => <img src={src} alt={`Slide ${index + 1}`} className="w-full" />}
       slidesPerViewConfig={{ 320: { slidesPerView: 1 }, 768: { slidesPerView: 1 } }}
       loop={true}
-      navigation={true}
+      navigation={true}      
     />
   );
-} 
+}
--- src/components/molecules/UniclubeSwiper.tsx
+++ src/components/molecules/UniclubeSwiper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, FreeMode } from "swiper/modules";
+import { Navigation, Pagination, FreeMode, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -15,7 +15,7 @@ interface UniclubeSwitchProps<T> {
   id: string;
   items: T[];
   renderItem: (item: T, index: number) => React.ReactNode;
-  slidesPerViewConfig?: { [width: number]: SwiperOptions;[ratio: string]: SwiperOptions; };
+  slidesPerViewConfig?: { [width: number]: SwiperOptions; [ratio: string]: SwiperOptions };
   slidesPerView?: number | "auto";
   slidesClass?: string;
   spaceBetween?: number;
@@ -24,6 +24,7 @@ interface UniclubeSwitchProps<T> {
   pagination?: boolean;
   freeMode?: boolean;
   className?: string;
+  autoplay?: boolean | { delay: number };
 }
 
 const commonClasses = "bg-primary-500 h-20 text-white p-3 rounded-full shadow-md z-10 transition-opacity duration-300";
@@ -41,6 +42,7 @@ export default function UniclubeSwitch<T>({
   pagination = false,
   freeMode = false,
   className = "",
+  autoplay
 }: UniclubeSwitchProps<T>) {
   const [isHovered, setIsHovered] = useState(false);
 
@@ -78,11 +80,12 @@ export default function UniclubeSwitch<T>({
       )}
 
       <Swiper
-        modules={[Navigation, Pagination, FreeMode]}
+        modules={[Navigation, Pagination, FreeMode, Autoplay]} // Adicionado Autoplay
         navigation={navigation ? { prevEl: `#prev_${id}`, nextEl: `#next_${id}` } : false}
         pagination={pagination ? { clickable: true } : false}
         freeMode={freeMode}
         loop={loop}
+        autoplay={autoplay ? (typeof autoplay === "boolean" ? { delay: 3000 } : autoplay) : false} // Corrigido autoplay
         spaceBetween={spaceBetween}
         slidesPerView={slidesPerView}
         breakpoints={slidesPerViewConfig}
